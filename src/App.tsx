58edb283b3cb7ca888b5fe60c75cf271b045fbd8
shy251227 <ckzz0213@gmail.com>
2025-09-30 00:35:51 +0900

    테스트용 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,89 @@
-import { Cover } from "./component/cover"
-import { Location } from "./component/location"
-import "./App.scss"
-import { BGEffect } from "./component/bgEffect"
-import { Invitation } from "./component/invitation"
-import { Calendar } from "./component/calendar"
-import { Gallery } from "./component/gallery"
-import { Information } from "./component/information"
-import { GuestBook } from "./component/guestbook"
-import { LazyDiv } from "./component/lazyDiv"
-import { ShareButton } from "./component/shareButton"
-import { STATIC_ONLY } from "./env"
+import { useEffect, useRef, useState } from "react";
+import { Cover } from "./component/cover";
+import { Location } from "./component/location";
+import "./App.scss";
+import { BGEffect } from "./component/bgEffect";
+import { Invitation } from "./component/invitation";
+import { Calendar } from "./component/calendar";
+import { Gallery } from "./component/gallery";
+import { Information } from "./component/information";
+import { GuestBook } from "./component/guestbook";
+import { LazyDiv } from "./component/lazyDiv";
+import { ShareButton } from "./component/shareButton";
+import { BGMPlayer } from "./component/bgmPlayer";
+import { STATIC_ONLY } from "./env";
+import Intro from "./component/intro";
 
-function App() {
-  return (
-    <div className="background">
-      <BGEffect />
-      <div className="card-view">
-        <LazyDiv className="card-group">
-          {/* 표지 */}
-          <Cover />
+const musicPath = `${
+  import.meta.env.BASE_URL
+}music/635_Fall_in_Love.mp3`;
 
-          {/* 모시는 글 */}
-          <Invitation />
-        </LazyDiv>
-
-        <LazyDiv className="card-group">
-          {/* 결혼식 날짜 (달력) */}
-          <Calendar />
-
-          {/* 겔러리 */}
-          <Gallery />
-        </LazyDiv>
-
-        <LazyDiv className="card-group">
-          {/* 오시는길 */}
-          <Location />
-        </LazyDiv>
-
-        <LazyDiv className="card-group">
-          {/* 마음 전하기 */}
-          <Information />
-          {/* 방명록 */}
-          {!STATIC_ONLY && <GuestBook />}
-        </LazyDiv>
-
-        <ShareButton />
-      </div>
-    </div>
-  )
+declare global {
+  interface Window {
+    Kakao: any;
+  }
 }
 
-export default App
+function App() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlay, setIsPlay] = useState(false);
+  const [showIntro, setShowIntro] = useState(true);
+
+  const handleEnter = () => {
+    setShowIntro(false);
+    audioRef.current?.play().then(() => {
+      setIsPlay(true);
+    });
+  };
+
+  const toggleBGM = () => {
+    if (!audioRef.current) return;
+    if (isPlay) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlay(!isPlay);
+  };
+
+  useEffect(() => {
+    const kakaoJavascriptKey = import.meta.env.VITE_KAKAO_SDK_JS_KEY;
+    if (kakaoJavascriptKey && window.Kakao && !window.Kakao.isInitialized()) {
+      window.Kakao.init(kakaoJavascriptKey);
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      {showIntro && <Intro onEnter={handleEnter} />}
+
+      <div className={`background ${showIntro ? 'blurred' : ''}`}>
+        <audio ref={audioRef} src={musicPath} loop preload="auto" />
+        <BGEffect />
+        <BGMPlayer isPlay={isPlay} onToggle={toggleBGM} />
+        <div className="card-view">
+          <LazyDiv className="card-group">
+            {/* ▼▼▼ 이 부분을 isCoverOff={false} 로 수정합니다! ▼▼▼ */}
+            <Cover isCoverOff={false} />
+            {/* ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ */}
+            <Invitation />
+          </LazyDiv>
+          <LazyDiv className="card-group">
+            <Calendar />
+            <Gallery />
+          </LazyDiv>
+          <LazyDiv className="card-group">
+            <Location />
+          </LazyDiv>
+          <LazyDiv className="card-group">
+            <Information />
+            {!STATIC_ONLY && <GuestBook />}
+          </LazyDiv>
+          <ShareButton />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,4 @@
+// 식장 주소 이름 , 번호 , 계좌번호
 import dayjs from "dayjs"
 import utc from "dayjs/plugin/utc"
 import timezone from "dayjs/plugin/timezone"
@@ -9,68 +10,69 @@ dayjs.locale("ko")
 
 export { dayjs }
 
-export const WEDDING_DATE = dayjs.tz("2024-08-24 13:00", "Asia/Seoul")
+export const WEDDING_DATE = dayjs.tz("2025-12-27 11:00", "Asia/Seoul")
 export const HOLIDAYS = [15]
 
-export const LOCATION = "서울대학교 연구공원 웨딩홀"
-export const LOCATION_ADDRESS = "서울시 관악구 관악로 1, 연구공원 본관 1층"
+export const LOCATION = "W시티 컨벤션 울산 웨딩홀"
+export const LOCATION_ADDRESS = "울산광역시 북구 진장 17길 7 별관7층"
 
 export const SHARE_ADDRESS = LOCATION
 export const SHARE_ADDRESS_TITLE = LOCATION
 
-export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
+export const WEDDING_HALL_POSITION = [35.561145, 129.355274]
 
-export const NMAP_PLACE_ID = 13321741
-export const KMAP_PLACE_ID = 8634826
+export const NMAP_PLACE_ID = 1946480978
+export const KMAP_PLACE_ID = 1099072690
 
-export const BRIDE_FULLNAME = "정지원"
-export const BRIDE_FIRSTNAME = "지원"
-export const BRIDE_TITLE = "장녀"
-export const BRIDE_FATHER = "정상원"
-export const BRIDE_MOTHER = "박윤정"
+//<정하윤> <정길모> <김차연>
+export const BRIDE_FULLNAME = "정하윤"
+export const BRIDE_FIRSTNAME = "하윤"
+export const BRIDE_TITLE = "딸"
+export const BRIDE_FATHER = "정길모"
+export const BRIDE_MOTHER = "김차연"
 export const BRIDE_INFO = [
   {
     relation: "신부",
     name: BRIDE_FULLNAME,
-    phone: "010-0000-0000",
+    phone: "010-9167-6675",
     account: "우리은행 0000000000000",
   },
   {
     relation: "신부 아버지",
     name: BRIDE_FATHER,
-    phone: "010-0000-0000",
+    phone: "010-9340-6675",
     account: "하나은행 00000000000",
   },
   {
     relation: "신부 어머니",
     name: BRIDE_MOTHER,
-    phone: "010-0000-0000",
+    phone: "010-4701-6675 ",
     account: "하나은행 00000000000000",
   },
 ]
 
-export const GROOM_FULLNAME = "남주호"
-export const GROOM_FIRSTNAME = "주호"
-export const GROOM_TITLE = "차남"
-export const GROOM_FATHER = "남현태"
-export const GROOM_MOTHER = "김현영"
+export const GROOM_FULLNAME = "차승훈"
+export const GROOM_FIRSTNAME = "승훈"
+export const GROOM_TITLE = "아들"
+export const GROOM_FATHER = "차호준"
+export const GROOM_MOTHER = "남미현"
 export const GROOM_INFO = [
   {
     relation: "신랑",
     name: GROOM_FULLNAME,
-    phone: "010-0000-0000",
+    phone: "010-5513-7567",
     account: "하나은행 00000000000000",
   },
   {
     relation: "신랑 아버지",
     name: GROOM_FATHER,
-    phone: "010-0000-0000",
+    phone: "010-6653-7567",
     account: "신한은행 000000000000",
   },
   {
     relation: "신랑 어머니",
     name: GROOM_MOTHER,
-    phone: "010-0000-0000",
+    phone: "010-5128-7567",
     account: "국민은행 000000000000",
   },
 ]
