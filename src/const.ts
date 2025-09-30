1e3dd172dd8c4dfa49480bd878b65c655d5d60e2
shy251227 <ckzz0213@gmail.com>
2025-09-30 10:31:03 +0900

    테스트용 수정

--- src/const.ts
+++ src/const.ts
@@ -24,55 +24,55 @@ export const WEDDING_HALL_POSITION = [35.561145, 129.355274]
 export const NMAP_PLACE_ID = 1946480978
 export const KMAP_PLACE_ID = 1099072690
 
-//<정하윤> <정길모> <김차연>
-export const BRIDE_FULLNAME = "정하윤"
-export const BRIDE_FIRSTNAME = "하윤"
+
+export const BRIDE_FULLNAME = "정HY"
+export const BRIDE_FIRSTNAME = "HY"
 export const BRIDE_TITLE = "딸"
-export const BRIDE_FATHER = "정길모"
-export const BRIDE_MOTHER = "김차연"
+export const BRIDE_FATHER = "JKM"
+export const BRIDE_MOTHER = "KCY"
 export const BRIDE_INFO = [
   {
     relation: "신부",
     name: BRIDE_FULLNAME,
-    phone: "010-9167-6675",
+    phone: "010-0000-0000",
     account: "우리은행 0000000000000",
   },
   {
     relation: "신부 아버지",
     name: BRIDE_FATHER,
-    phone: "010-9340-6675",
+    phone: "010-0000-0000",
     account: "하나은행 00000000000",
   },
   {
     relation: "신부 어머니",
     name: BRIDE_MOTHER,
-    phone: "010-4701-6675 ",
+    phone: "010-0000-0000 ",
     account: "하나은행 00000000000000",
   },
 ]
 
-export const GROOM_FULLNAME = "차승훈"
-export const GROOM_FIRSTNAME = "승훈"
+export const GROOM_FULLNAME = "차SH"
+export const GROOM_FIRSTNAME = "SH"
 export const GROOM_TITLE = "아들"
-export const GROOM_FATHER = "차호준"
-export const GROOM_MOTHER = "남미현"
+export const GROOM_FATHER = "CHJ"
+export const GROOM_MOTHER = "NMH"
 export const GROOM_INFO = [
   {
     relation: "신랑",
     name: GROOM_FULLNAME,
-    phone: "010-5513-7567",
+    phone: "010-0000-0000",
     account: "하나은행 00000000000000",
   },
   {
     relation: "신랑 아버지",
     name: GROOM_FATHER,
-    phone: "010-6653-7567",
+    phone: "010-0000-0000",
     account: "신한은행 000000000000",
   },
   {
     relation: "신랑 어머니",
     name: GROOM_MOTHER,
-    phone: "010-5128-7567",
+    phone: "010-0000-0000",
     account: "국민은행 000000000000",
   },
 ]
